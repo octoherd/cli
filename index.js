6487f1e9ef14669a3d7cba02c7c201e4199ddf03
Gregor Martynus <39992+gr2m@users.noreply.github.com>
2021-02-25 13:04:45 -0800

    WIP

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import { appendFileSync } from "fs";
 
 import { Octokit } from "@octoherd/octokit";
 import chalk from "chalk";
+import tempy from "tempy";
 
 import { cache as octokitCachePlugin } from "./lib/octokit-plugin-cache.js";
 import { resolveRepositories } from "./lib/resolve-repositories.js";
@@ -15,61 +16,66 @@ const levelColor = {
   error: chalk.bgRed.white.bold,
 };
 
-const OctokitWithDefaults = Octokit.defaults({
-  userAgent: ["octoherd-cli", VERSION].join("/"),
-  octoherd: {
-    debug: true,
-    onLogMessage(level, message, additionalData) {
-      console.log(
-        levelColor[level](" " + level.toUpperCase() + " "),
-        Object.keys(additionalData).length
-          ? `${message} ${chalk.gray(JSON.stringify(additionalData))}`
-          : message
-      );
-    },
-    onLogData(data) {
-      appendFileSync("debug.log", JSON.stringify(data));
-    },
-  },
-});
-
 /**
  * Find all releases in a GitHub repository or organization after a specified date
  *
  * @param {object} options
- * @param {string} options.token Personal Access Token: Requires the "public_repo" scope for public repositories, "repo" scope for private repositories.
- * @param {string} options.script Path to script to run against a repository
- * @param {string} options.repos Array of repository names in the form of "repo-owner/repo-name". To match all repositories for an owner, pass "repo-owner/*"
- * @param {boolean} options.cache Cache responses for debugging
+ * @param {string} options.octoherdToken Personal Access Token: Requires the "public_repo" scope for public repositories, "repo" scope for private repositories.
+ * @param {string} options.octoherdScript Path to script to run against a repository
+ * @param {string} options.octoherdCache Array of repository names in the form of "repo-owner/repo-name". To match all repositories for an owner, pass "repo-owner/*"
+ * @param {boolean} options.octoherdRepos Cache responses for debugging
  */
 export async function octoherd(
   options = {
-    cache: false,
-    repos: [],
+    octoherdCache: false,
+    octoherdRepos: [],
   }
 ) {
-  const { token, script, repos, cache, ...userOptions } = options;
-  const CliOctokit = cache
-    ? OctokitWithDefaults.plugin(octokitCachePlugin)
-    : OctokitWithDefaults;
+  const {
+    octoherdToken,
+    octoherdScript,
+    octoherdCache,
+    octoherdRepos,
+    ...userOptions
+  } = options;
+  const tmpLogFile = tempy.file({ extension: "ndjson.log" });
+
+  const CliOctokit = octoherdCache
+    ? Octokit.plugin(octokitCachePlugin)
+    : Octokit;
   const octokit = new CliOctokit({
-    auth: token,
+    auth: octoherdToken,
+    userAgent: ["octoherd-cli", VERSION].join("/"),
+    octoherd: {
+      debug: true,
+      onLogMessage(level, message, additionalData) {
+        console.log(
+          levelColor[level](" " + level.toUpperCase() + " "),
+          Object.keys(additionalData).length
+            ? `${message} ${chalk.gray(JSON.stringify(additionalData))}`
+            : message
+        );
+      },
+      onLogData(data) {
+        appendFileSync(tmpLogFile, JSON.stringify(data));
+      },
+    },
   });
 
   let userScript;
-  const path = resolve(process.cwd(), script);
+  const path = resolve(process.cwd(), octoherdScript);
 
   try {
     userScript = (await import(path)).script;
   } catch (error) {
-    throw new Error(`[octoherd] ${script} script could not be found`);
+    throw new Error(`[octoherd] ${octoherdScript} script could not be found`);
   }
 
   if (!userScript) {
     throw new Error(`[octoherd] no "script" exported at ${path}`);
   }
 
-  if (repos.length === 0) {
+  if (octoherdCache.length === 0) {
     throw new Error("[octoherd] No repositories provided");
   }
 
@@ -78,12 +84,27 @@ export async function octoherd(
     octokit,
   };
 
-  const repositories = await resolveRepositories(state, repos);
+  try {
+    const repositories = await resolveRepositories(state, octoherdRepos);
 
-  for (const repository of repositories) {
-    octokit.log.info("Running %s on %s...", script, repository.full_name);
-    await userScript(octokit, repository, userOptions);
+    for (const repository of repositories) {
+      octokit.log.info(
+        "Running %s on %s...",
+        octoherdScript,
+        repository.full_name
+      );
+      await userScript(octokit, repository, userOptions);
+    }
+
+    console.log("");
+    console.log(levelColor.info(" DONE "), `Log file written to ${tmpLogFile}`);
+  } catch (error) {
+    octokit.log.error(error);
+    console.log("");
+    console.log(
+      levelColor.error(" DONE "),
+      `Log file written to ${tmpLogFile}`
+    );
+    process.exit(1);
   }
-
-  octokit.log.info("done");
 }
