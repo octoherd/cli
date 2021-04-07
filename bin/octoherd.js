7b975b34d033741359a231643c4bc63a836e16ee
Gregor Martynus <39992+gr2m@users.noreply.github.com>
2021-04-07 16:15:19 -0700

    fix: remove unneeded versions from `octoherd` binary help epilog

--- bin/octoherd.js
+++ bin/octoherd.js
@@ -9,9 +9,7 @@ import { octoherd } from "../index.js";
 import { VERSION } from "../version.js";
 import runCommand from "./commands/run.js";
 
-const EPILOG = chalk.gray(`${VERSIONS}
-
-Questions? Ideas? Feedback?
+const EPILOG = chalk.gray(`Questions? Ideas? Feedback?
 https://github.com/octoherd/octoherd/discussions
   
 Copyright 2020-${new Date().getFullYear()} Octoherd Contributors`);
