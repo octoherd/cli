a926cee7f1b2805e02b2f1ec74d21e8d0fa0e213
Gregor Martynus <39992+gr2m@users.noreply.github.com>
2021-04-07 14:56:59 -0700

    refactor: move CLI options into run command

--- bin/commands/run.js
+++ bin/commands/run.js
@@ -4,12 +4,51 @@ import chalk from "chalk";
 import { VERSION as OctokitVersion } from "@octoherd/octokit";
 
 import { VERSION } from "../../version.js";
-import { cliOptions } from "../cli-options.js";
 
 const VERSIONS = `
 @octoherd/cli:     v${VERSION}
 @octoherd/octokit: v${OctokitVersion}
 Node.js:           ${process.version}, ${process.platform} ${process.arch}`.trim();
+
+/** @type { {[key: string]: import("yargs").Options} } */
+const options = {
+  "octoherd-token": {
+    description:
+      'Requires the "public_repo" scope for public repositories, "repo" scope for private repositories.',
+    demandOption: true,
+    type: "string",
+    alias: "T",
+  },
+  "octoherd-script": {
+    description: "Path to *.js script. Must be an ES Module.",
+    demandOption: true,
+    type: "string",
+    alias: "S",
+  },
+  "octoherd-repos": {
+    description:
+      "One or multiple repositories in the form of 'repo-owner/repo-name'. 'repo-owner/*' will find all repositories for one owner. '*' will find all repositories the user has access to",
+    type: "string",
+    array: true,
+    demandOption: true,
+    alias: "R",
+  },
+  "octoherd-cache": {
+    description:
+      "Cache responses for debugging. Creates a ./cache folder if flag is set. Override by passing custom path",
+    type: "string",
+  },
+  "octoherd-debug": {
+    description: "Show debug logs",
+    type: "boolean",
+    default: false,
+  },
+  "octoherd-bypass-confirms": {
+    description: "Bypass prompts to confirm mutating requests",
+    type: "boolean",
+    default: false,
+  },
+};
 
 /** @type import('yargs').CommandModule */
 const runCommand = {
@@ -19,7 +58,7 @@ const runCommand = {
     yargs
       .usage("Usage: $0 run [options]")
       .example("$0 run -T $TOKEN -S path/to/script.js -R octoherd/cli", "")
-      .options(cliOptions)
+      .options(options)
       .version(VERSIONS)
       .coerce("octoherd-script", async (script) => {
         if (!script) return;
@@ -43,7 +82,7 @@ const runCommand = {
 
         return scriptModule.script;
       }),
-  handler: (args) => {
+  handler: () => {
     console.log(
       `\n${chalk.bold("Running @octoherd/cli v%s")} ${chalk.gray(
         "(@octoherd/octokit v%s, Node.js: %s, %s %s)"
