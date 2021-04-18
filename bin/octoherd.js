45b127a793bf81d5c7a2919529b84e141847e132
Gregor Martynus <39992+gr2m@users.noreply.github.com>
2021-04-18 15:58:19 -0700

    feat: `octoherd run`, use `--octoherd-*` flags for script and repos (#24)
    BREAKING CHANGE: `octoherd` can no longer be run without a command. Use `octoherd run` instead
    
    BREAKING CHANGE: the script and repos arguments are now flags.
    
    Instead of
    
    ```
    octoherd --octoherd-token $TOKEN script.js octoherd/octoherd
    ```
    
    do
    
    ```
    octoherd run --octoherd-token $TOKEN --octoherd-script script.js --octoherd-repos octoherd/octoherd
    ```
    
    or use the shortcut aliases
    
    ```
    octoherd run -T $TOKEN -S script.js -R octoherd/octoherd
    ```

--- bin/octoherd.js
+++ bin/octoherd.js
@@ -2,67 +2,27 @@
 
 import yargs from "yargs";
 import { hideBin } from "yargs/helpers";
-import { resolve } from "path";
-import { VERSION as OctokitVersion } from "@octoherd/octokit";
+
 import chalk from "chalk";
 
 import { octoherd } from "../index.js";
 import { VERSION } from "../version.js";
-import { cliOptions } from "./cli-options.js";
+import runCommand from "./commands/run.js";
 
-const argv = yargs(hideBin(process.argv))
-  .usage("Usage: $0 [options] [script] [repos...]")
-  .example(
-    "$0 --token 0123456789012345678901234567890123456789 octokit/rest.js"
-  )
-  .command("$0 [script] [repos...]", "", (yargs) => {
-    yargs.positional("script", {
-      demandOption: true,
-      describe: "Path to *.js script. Must be an ES Module.",
-    });
-    yargs.positional("repos", {
-      demandOption: true,
-      describe:
-        "One or multiple arrays in the form of 'repo-owner/repo-name'. 'repo-owner/*' will find all repositories for one owner. '*' will find all repositories the user has access to",
-      default: [],
-    });
-  })
-  .options(cliOptions)
+const EPILOG = chalk.gray(`Questions? Ideas? Feedback?
+https://github.com/octoherd/octoherd/discussions
+  
+Copyright 2020-${new Date().getFullYear()} Octoherd Contributors`);
+
+const argv = await yargs(hideBin(process.argv))
+  .command(runCommand)
+  .demandCommand()
   .version(VERSION)
-  .epilog(`copyright 2020-${new Date().getFullYear()}`).argv;
-
-const { _, $0, script, repos, ...options } = argv;
-
-console.log(
-  `\n${chalk.bold("Running @octoherd/cli v%s")} ${chalk.gray(
-    "(@octoherd/octokit v%s, Node.js: %s, %s %s)"
-  )}`,
-  VERSION,
-  OctokitVersion,
-  process.version,
-  process.platform,
-  process.arch
-);
-
-let octoherdScript;
-const path = resolve(process.cwd(), script);
-
-console.log("Loading script at %s\n", script);
+  .epilog(EPILOG).argv;
 
 try {
-  octoherdScript = (await import(path)).script;
+  await octoherd(argv);
 } catch (error) {
-  console.error(error.stack);
-  throw new Error(`[octoherd] ${script} script could not be found`);
+  console.error(error);
+  process.exit(1);
 }
-
-if (!octoherdScript) {
-  throw new Error(`[octoherd] no "script" exported at ${path}`);
-}
-
-octoherd({ ...options, octoherdScript, octoherdRepos: repos }).catch(
-  (error) => {
-    console.error(error);
-    process.exit(1);
-  }
-);
