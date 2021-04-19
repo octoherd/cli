4ac8b4e245f72a4a0740e829db97c627ea069f73
Gregor Martynus <39992+gr2m@users.noreply.github.com>
2021-04-19 10:27:15 -0700

    feat: allow to use `*` at the beginning and the middle of repository name patterns (#37)

--- tests/resolve-repositories.test.js
+++ tests/resolve-repositories.test.js
@@ -75,54 +75,134 @@ withOrg("when requesting all the repositories", async () => {
   equal(resolvedRepos, mockedResponse);
 });
 
-withOrg("when requesting all the repositories under a pattern", async () => {
-  const org = "octoherd";
-  const repo = "script-*";
-  const octokit = new Octokit({
-    auth: process.env.GITHUB_TOKEN,
-  });
+withOrg(
+  "when requesting all the repositories with pattern at end",
+  async () => {
+    const org = "octoherd";
+    const repo = "script-*";
+    const octokit = new Octokit({
+      auth: process.env.GITHUB_TOKEN,
+    });
 
-  const repositories = [`${org}/${repo}`];
+    const repositories = [`${org}/${repo}`];
 
-  const scriptRepos = [
-    { name: "script-close-renovate-dashboard-issues" },
-    { name: "script-create-repositories-from-script-folders" },
-    { name: "script-find-releases" },
-    { name: "script-remove-dependabot" },
-    { name: "script-remove-required-ci-check" },
-    { name: "script-setup-renovate" },
-    { name: "script-star-or-unstar" },
-    { name: "script-sync-branch-protections" },
-    { name: "script-add-octoherd-cli-to-script" },
-    { name: "script-hello-world" },
-  ];
+    const scriptRepos = [
+      { name: "script-close-renovate-dashboard-issues" },
+      { name: "script-create-repositories-from-script-folders" },
+      { name: "script-find-releases" },
+      { name: "script-remove-dependabot" },
+      { name: "script-remove-required-ci-check" },
+      { name: "script-setup-renovate" },
+      { name: "script-star-or-unstar" },
+      { name: "script-sync-branch-protections" },
+      { name: "script-add-octoherd-cli-to-script" },
+      { name: "script-hello-world" },
+    ];
 
-  const nonScriptRepos = [
-    { name: "cli" },
-    { name: "octoherd" },
-    { name: ".github" },
-    { name: "octokit" },
-    { name: "create-octoherd-script }" },
-  ];
+    const nonScriptRepos = [
+      { name: "cli" },
+      { name: "octoherd" },
+      { name: ".github" },
+      { name: "octokit" },
+      { name: "create-octoherd-script }" },
+    ];
 
-  simple.mock(octokit, "request").resolveWith(undefined);
+    simple.mock(octokit, "request").resolveWith(undefined);
 
-  simple.mock(octokit.paginate, "iterator").returnWith({
-    async *[Symbol.asyncIterator]() {
-      yield { data: [...scriptRepos, ...nonScriptRepos] };
-    },
-  });
+    simple.mock(octokit.paginate, "iterator").returnWith({
+      async *[Symbol.asyncIterator]() {
+        yield { data: [...scriptRepos, ...nonScriptRepos] };
+      },
+    });
 
-  const resolvedRepos = await resolveRepositories(
-    {
-      log: console,
-      octokit,
-    },
-    repositories
-  );
+    const resolvedRepos = await resolveRepositories(
+      {
+        log: console,
+        octokit,
+      },
+      repositories
+    );
 
-  equal(resolvedRepos, scriptRepos);
-});
+    equal(resolvedRepos, scriptRepos);
+  }
+);
+
+withOrg(
+  "when requesting all the repositories with pattern at end",
+  async () => {
+    const org = "octoherd";
+    const repo = "*-test";
+    const octokit = new Octokit({
+      auth: process.env.GITHUB_TOKEN,
+    });
+
+    const repositories = [`${org}/${repo}`];
+
+    const testRepos = [
+      { name: "one-test" },
+      { name: "two-test" },
+      { name: "three-test" },
+    ];
+
+    const nonTestRepos = [{ name: "foo" }, { name: "bar" }];
+
+    simple.mock(octokit, "request").resolveWith(undefined);
+
+    simple.mock(octokit.paginate, "iterator").returnWith({
+      async *[Symbol.asyncIterator]() {
+        yield { data: [...testRepos, ...nonTestRepos] };
+      },
+    });
+
+    const resolvedRepos = await resolveRepositories(
+      {
+        log: console,
+        octokit,
+      },
+      repositories
+    );
+
+    equal(resolvedRepos, testRepos);
+  }
+);
+withOrg(
+  "when requesting all the repositories with pattern in the middle",
+  async () => {
+    const org = "octoherd";
+    const repo = "middle-*-test";
+    const octokit = new Octokit({
+      auth: process.env.GITHUB_TOKEN,
+    });
+
+    const repositories = [`${org}/${repo}`];
+
+    const testRepos = [
+      { name: "middle-one-test" },
+      { name: "middle-two-test" },
+      { name: "middle-three-test" },
+    ];
+
+    const nonTestRepos = [{ name: "foo" }, { name: "bar" }];
+
+    simple.mock(octokit, "request").resolveWith(undefined);
+
+    simple.mock(octokit.paginate, "iterator").returnWith({
+      async *[Symbol.asyncIterator]() {
+        yield { data: [...testRepos, ...nonTestRepos] };
+      },
+    });
+
+    const resolvedRepos = await resolveRepositories(
+      {
+        log: console,
+        octokit,
+      },
+      repositories
+    );
+
+    equal(resolvedRepos, testRepos);
+  }
+);
 
 withUser("when single repository exists", async () => {
   const owner = "gr2m";
